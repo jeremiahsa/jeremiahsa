30cdb31543e21803ed6c887e20da14dc5bd43292
jeremiahsa <jeremiahsa@gmail.com>
2012-06-15 00:38:04 -0500

    Added class Video to the project

--- augustAsh/index.php
+++ augustAsh/index.php
@@ -1,3 +1,30 @@
 <?php
 
+class Video {
+	$url ;
+	$title ;
+	
+	public function get_videos_by_submission_order() {
+		
+	}
+	
+	public function get_top_10_by_ratings() {
+		
+	}
+	
+	public function get_top_10_by_views() {
+		
+	}
+	
+	public function post_new_URL() {
+		
+	}
+	
+	public function update_video_rating() {
+		
+	}
+	
+}
+
+
 ?>
