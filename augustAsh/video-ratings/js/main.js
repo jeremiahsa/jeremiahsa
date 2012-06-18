9caafa697ffd8017b84dfce939ca660b53ef3612
jeremiahsa <jeremiahsa@gmail.com>
2012-06-18 18:16:17 -0500

    Top 10 results loaded with ajax

--- augustAsh/video-ratings/js/main.js
+++ augustAsh/video-ratings/js/main.js
@@ -4,35 +4,44 @@ $(document).ready(function() {
 // 		Show/Hide the top 10 section with the buttons 
 //		Also handles the submission and injection of the top 10 results
 //		Into the appropriate div upon click of the respective button
-/*
 	
 	$('#rating').hide();
 	$('#views').hide();
 	$('#submission').hide();
 	
+
+	
 	$('#by-rating').click(function() {
 		$('#views').hide();
 		$('#submission').hide();
-		$.post('top-10.php', function(data) {
-			window.location = 'top-10.php';
-			$('#rating').fadeIn(data);
-		});
-		return false;
+		$('#rating').fadeIn();
+		$.ajax({
+				url:'top-10.php',
+				method: 'POST',
+				data: 'byratings=123',
+				success: function(data) {
+					$('#rating').empty();
+					$('#rating').append(data);
+				}
+			}); // end ajax
+
+		
+			return false;
 	});
 	
 	$('#by-views').click(function() {
 		$('#views').fadeIn();	
 		$('#rating').hide();
 		$('#submission').hide();
-		return false;
+		return true;
 	});
 	
 	$('#by-submission').click(function() {
 		$('#rating').hide();
 		$('#views').hide();
 		$('#submission').fadeIn();	
-		return false;
+		return true;
 	});
 
-*/
+
 });
