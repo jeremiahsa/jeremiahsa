a55c6f519d9aa5c2f9e3443cfe68a114b32755f8
jeremiahsa <jeremiahsa@gmail.com>
2012-06-18 13:18:58 -0500

    Form properly POSTS new video url

--- augustAsh/video-ratings/add-url.php
+++ augustAsh/video-ratings/add-url.php
@@ -14,36 +14,48 @@ if (isset($_POST['title'])) {
 	
  	$title=$_POST['title'];
   	$url=$_POST['url'];
+	$slug = $_POST['slug'];
 
 //
 // Convert form data into JSON format for submission to RESTful API
 //	
+
+//$array = array("title" => $title, "url" => $url);
+//echo	$json_string = json_encode($array);
+//echo "<br>";
 	
-	$json_string = '{ "title":"'.$title.'", "url":"'.$url.'"  }';
+/*echo $json_string = '{ 
+		\'title\' : \''.$title.'\', 
+		\'url\' : \''.$url.'\', 
+		\'slug\' : \''.$slug.'\' 
+		}';
+*/
+
+echo $json_string = 'title='.$title.'&url='.$url;
 
 //
 // Submit JSON data to API
 //
 
-$ashuri = 'https://ashapi.heroku.com/videos/';
+$ashuri = 'https://ashapi.heroku.com/videos';
 
 $ch = curl_init($ashuri);
 curl_setopt($ch, CURLOPT_USERPWD, "jsandahl:d598d6e400fb796ab23e39288bfd63d0");
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, POST);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($ch, CURLOPT_HTTPHEADER, Array("Content-Type:application/json"));
-curl_setopt($ch, CURLOPT_POSTFIELDS, $json_string);
+curl_setopt($ch, CURLOPT_HEADER, true);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, "title=$title&url=$url");
 $response = curl_exec($ch);
 
 if (curl_errno($ch)) {
 	$error = curl_error($ch);
-	echo "errors:" . $error;	
+	echo "<br>errors:" . $error. "<br>";	
 } else {
-	echo "no errors<br>";
+	echo "<br>no curl errors<br>";
 }
 $status = curl_getinfo($ch);
 echo "<br>status is:" . print_r($status) ."<br>";
-echo "<br>response is:" . $response;
 curl_close($ch);
 
 
