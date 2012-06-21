51a7406ab7a2636d90e8260d2ecc8197a4ede2d7
jeremiahsa <jeremiahsa@gmail.com>
2012-06-21 15:44:25 -0500

    API does not provide for view counting to be POSTed to server. Vote information is accurately retrieved, but not successfully posted.

--- augustAsh/video-ratings/views.php
+++ augustAsh/video-ratings/views.php
@@ -7,26 +7,32 @@
 <?php
 
 function getViews($ashuri, $video_id) {
+	echo "ashuri:".$ashuri."video_id:".$video_id;
 	$ch = curl_init($ashuri);
 	curl_setopt($ch, CURLOPT_USERPWD, "jsandahl:d598d6e400fb796ab23e39288bfd63d0");
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-	curl_setopt($ch, CURLOPT_HEADER, false);
+	curl_setopt($ch, CURLOPT_HEADER, true);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	$response = curl_exec($ch);
-	
+	echo $response = curl_exec($ch);
+	$array = json_decode($response);
+	echo $views = $array['$video_id']->view_count;
 	return $views;
 }
 
-function connectToAPI() {
+function connectToAPI($ashapi, $video_id, $currentViews) {
+	echo "<br>ashuri: ".$ashapi." video_id: ".$video_id."current views: ".$currentViews;
+	
+	$ch = curl_init($ashapi);
 	curl_setopt($ch, CURLOPT_USERPWD, "jsandahl:d598d6e400fb796ab23e39288bfd63d0");
 	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, POST);
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-	curl_setopt($ch, CURLOPT_HEADER, false);
+	curl_setopt($ch, CURLOPT_HEADER, true);
 	curl_setopt($ch, CURLOPT_POST, 1);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, "id=$video_id&views=$views");
+	curl_setopt($ch, CURLOPT_POSTFIELDS, "id=$video_id&view_count=$currentViews");
+	echo $response = curl_exec($ch);
 }
-
+/*
 if (isset($_GET['id'])) {
 	$video_id = $_GET['id'];
 	
@@ -42,11 +48,12 @@ if (isset($_GET['id'])) {
 	setcookie($video_id, $expire);
 		
 	}
-}
+}*/
+$video_id=147;
 
 $ashapi = 'https://ashapi.heroku.com/videos/'.$video_id.'/views';
-$currentViews = getViews($ashapi, $video_id);
-connectToAPI($ashapi);
+echo "<br>".$currentViews = getViews($ashapi, $video_id) + 1;
+connectToAPI($ashapi, $video_id, $currentViews);
 
 
 ?>
